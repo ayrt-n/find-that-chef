cac9433778275772a4e7e1e6940be255dc76f007
ayrton <ayrton.parkinson1@gmail.com>
2024-05-01 05:56:37 -0400

    Change season column to string

--- db/migrate/20240427140416_create_appearances.rb
+++ db/migrate/20240427140416_create_appearances.rb
@@ -1,7 +1,7 @@
 class CreateAppearances < ActiveRecord::Migration[7.0]
   def change
     create_table :appearances do |t|
-      t.integer :season
+      t.string :season
       t.belongs_to :chef, index: true, foreign_key: true
       t.belongs_to :show, index: true, foreign_key: true
 
