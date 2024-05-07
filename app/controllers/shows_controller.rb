b65e365e781e1c08468c757ffb9f5c28b4ba82c8
ayrton <ayrton.parkinson1@gmail.com>
2024-05-07 07:06:36 -0400

    Update chef query

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,6 +2,6 @@ class ShowsController < ApplicationController
   def show
     @show = Show.find(params[:id])
     @seasons = @show.seasons
-    @pagy, @chefs = pagy(@show.chefs.includes(:restaurants))
+    @pagy, @chefs = pagy(@show.chefs.order(:first_name, :last_name).includes(:restaurants))
   end
 end
