10cef3ce3b7fec31658de0efbe58e0f6c6e326dd
ayrton <ayrton.parkinson1@gmail.com>
2024-05-10 07:42:06 -0400

    Improve accessibility of dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,16 +1,64 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "button"]
+  static targets = ["menu", "button", "menuItem"]
+  static values = { open: { type: Boolean, default: false } }
+
+  connect() {
+    document.addEventListener("turbo:before-cache", this.beforeCache.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:before-cache", this.beforeCache.bind(this));
+  }
 
   toggle() {
-    this.menuTarget.style.display = this.menuTarget.style.display === 'none' ? '' : 'none';
+    this.openValue = !this.openValue
+    this.menuTarget.classList.toggle("hidden");
+  }
+
+  show() {
+    this.openValue = true;
+    this.menuTarget.classList.remove("hidden");
   }
 
   hide(event) {
-    if (this.buttonTarget.contains(event.target) || this.menuTarget.contains(event.target)) {
-      return;
+    if (this.openValue && (this.element.contains(event.target) === false || event.key === 'Escape')) {
+      this.openValue = false;
+      this.menuTarget.classList.add("hidden");
     }
-    this.menuTarget.style.display = 'none'
+  }
+
+  openValueChanged() {
+    if (this.openValue === true && this.hasMenuItemTarget) {
+      this.menuItemTargets[0].focus();
+    }
+  }
+
+  nextItem(event) {
+    event.preventDefault();
+    this.menuItemTargets[this.nextIndex].focus();
+  }
+
+  prevItem(event) {
+    event.preventDefault();
+    this.menuItemTargets[this.prevIndex].focus();
+  }
+
+  get currentItemIndex() {
+    return this.menuItemTargets.indexOf(document.activeElement);
+  }
+
+  get nextIndex() {
+    return Math.min(this.currentItemIndex + 1, this.menuItemTargets.length - 1);
+  }
+
+  get prevIndex() {
+    return Math.max(this.currentItemIndex - 1, 0);
+  }
+
+  beforeCache() {
+    this.openValue = false;
+    this.menuTarget.classList.add("hidden");
   }
 }
